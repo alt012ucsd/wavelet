78f3514484190ca608dfbf1fd765b13568b6cbfe
alt012ucsd <alt012@ucsd.edu>
2023-04-12 18:32:22 -0700

    Create SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+
+public class SearchEngine implements URLHandler{
+
+    public String handleRequest(URI url){
+        ArrayList<String> list = new ArrayList<String>();
+        String string = url.getPath();
+
+        if 
+    }
+    
+}
